b0aafb7a0395a2df8e98a4d65ab49848da5d9755
Hiep Trinh <hieptq.social@gmail.com>
2020-09-16 23:17:59 +1000

    Update few validation

--- src/forms/ItemManagement/Property/validate.js
+++ src/forms/ItemManagement/Property/validate.js
@@ -1,5 +1,7 @@
 import * as Yup from 'yup'
 
 export default Yup.object({
-  itemName: Yup.string().max(50, 'Must be 50 characters or less').required('Required'),
+  itemName: Yup.string().max(50, 'Không nhập quá 50 từ').required('Phải nhập dữ liệu'),
+  color: Yup.string().max(20, "Không nhập quá 20 từ"),
+  year: Yup.number().required("Phải nhập số").typeError("Phải nhập số"),
 })
